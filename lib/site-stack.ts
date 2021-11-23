e2f1286d84be373b82948eb76df705637d24e9e1
Kyle Mueller <Mueller.Kyle@principal.com>
2021-11-23 00:08:09 -0600

    Add bucket deployment

--- lib/site-stack.ts
+++ lib/site-stack.ts
@@ -1,9 +1,11 @@
 import * as cdk from "@aws-cdk/core";
 import { BlockPublicAccess, Bucket } from "@aws-cdk/aws-s3";
+import { BucketDeployment, Source } from "@aws-cdk/aws-s3-deployment";
 import { Distribution } from "@aws-cdk/aws-cloudfront";
 import { S3Origin } from "@aws-cdk/aws-cloudfront-origins";
 import { ARecord, PublicHostedZone, RecordTarget } from "@aws-cdk/aws-route53";
 import { CloudFrontTarget } from "@aws-cdk/aws-route53-targets";
+import * as path from "path";
 
 export class SiteStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -11,6 +13,11 @@ export class SiteStack extends cdk.Stack {
 
     const bucket = new Bucket(this, "static-site-bucket", {
       blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+    });
+
+    new BucketDeployment(this, "static-site-bucket-deployment", {
+      sources: [Source.asset(path.resolve(__dirname, "./../src/"))],
+      destinationBucket: bucket,
     });
 
     const distribution = new Distribution(this, "dist", {
